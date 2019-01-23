d601143017b8d530df63bf357ed8f2d66649e3d4
Jacob Warren <jacob.paul.warren@gmail.com>
2019-01-22 20:53:06 -0600

    remove Ecto dependency

--- examples/ecto_poll_queue/config/postgres.exs
+++
@@ -1,8 +0,0 @@
-use Mix.Config
-
-config :ecto_poll_queue_example, EctoPollQueueExample.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  database: "honeydew_test",
-  username: "postgres",
-  password: "",
-  hostname: "localhost"
--- mix.exs
+++ mix.exs
@@ -38,11 +38,9 @@ defmodule Honeydew.Mixfile do
 
   defp deps do
     [
-      {:ecto, "~> 2.0", optional: true, only: [:dev, :prod]},
       {:riakc, ">= 2.4.1", only: :dev},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
-      # {:eflame, git: "git@github.com:slfritchie/eflame", only: :dev},
     ]
   end
 
